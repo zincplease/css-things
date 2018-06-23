c22b4f17b9f2a689b3dc04dd041d620c770f56d9
Nikita Bekish <35031714+zincplease@users.noreply.github.com>
2018-06-23 16:04:47 +0300

    4 lessons

---
+++ envelope/js/js.js
@@ -0,0 +1,4 @@
+$('.envelope').on('click', function(e) {
+                  $('.envelope_top').toggleClass('envelope_top_close');
+                  $('.paper').toggleClass('paper_close');
+                  })
